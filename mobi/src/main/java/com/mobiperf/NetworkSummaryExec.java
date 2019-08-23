e616fba781efdbd541463307446b0cf8fbf1bcf6
Meluleki Dube <mellarmellah@gmail.com>
2019-08-23 12:07:29 +0200

    Cheanged the time interval between collection of network summary to a day

--- mobi/src/main/java/com/mobiperf/NetworkSummaryExec.java
+++ mobi/src/main/java/com/mobiperf/NetworkSummaryExec.java
@@ -6,13 +6,13 @@ import java.util.concurrent.TimeUnit;
 
 public class NetworkSummaryExec {
     private final static int INIT_DELAY=1;
-    private final static int PERIOD = 2;
+    private final static int PERIOD_MINUTES = 60*24;
     private final static int THREAD_POOL_SIZE = 1;
     private final static ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(THREAD_POOL_SIZE);
     private static boolean isStarted=false;
     public static void startCollector(){
         if(!isStarted) {
-            scheduler.scheduleAtFixedRate(new NetworkSummaryCollector(), INIT_DELAY, PERIOD, TimeUnit.MINUTES);
+            scheduler.scheduleAtFixedRate(new NetworkSummaryCollector(), INIT_DELAY, PERIOD_MINUTES, TimeUnit.MINUTES);
             System.out.println("Collector Has Started");
             isStarted=true;
         }
